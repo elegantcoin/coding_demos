878dd3035d7381489ae9ff753b4916e73c8fdc94
elegantcoin <1041244359@qq.com>
2020-10-31 00:19:13 +0800

    创建C++程序演示

---
+++ coding_demo/coding_demo.cpp
@@ -0,0 +1,38 @@
+#include "coding_demo.h"
+#include<cstring>
+#include<bitset> //for ¶þ½øÖÆ
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+using namespace std;
+
+
+// () x++  --y / % + >  & |  &&  ||  ?:  ,
+// cout<<bitset<8>(63)<<endl; 00111111
+// cout<<std::dec<<00000010<<endl;  8
+//---------------------------------------func---------------------------------------
+//
+
+
+
+//---------------------------------------func---------------------------------------
+
+void func()
+{
+	int a=10,b=10,c;
+	c=++a>=---b++;
+	printf("Value of c is %d\n",c);
+
+	
+}
+
+
+
+
+//---------------------------------------func---------------------------------------
+
+void coding_demo()
+{
+	func();
+	char();
+}
